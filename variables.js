11b7ee541c59119070ce8bcd65f81d0bc1778ac2
RUSHILPATEL33 <rushilp274@gmail.com>
2025-01-24 22:40:25 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,13 @@
+const accountID = 14453
+let accountEmail = "rushilp274@gmail.com"
+var accountPassword = "12345"
+accountCity = "Ahemdabad"
+
+// accountID = 2
+
+accountEmail = "hc@hcgmail.com"
+accountPassword = "2121"
+accountCity = "Vadodara"
+console.log(accountID);
+console.table([accountID,accountEmail,accountPassword,accountCity])
+
